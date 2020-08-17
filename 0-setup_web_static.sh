2f25a24ec1fbdf6fc8603f89b62988c3abdae9d1
saidskander <askander199h@outlook.com>
2020-08-17 22:11:25 +0100

    Done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,36 +1,29 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static.
-#!/usr/bin/env bash
-# sets up your web servers for the deployment of web_static.
-apt-get -y update
-apt-get -y install nginx
+
+apt-get update
+apt-get install -y nginx
+
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-
-echo "<html>
-         <head>
-         </head>
-         <body>
-             Holberton School
-         </body>
-      </html>" > /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
+
 printf %s "server {
-    listen 80;
+    listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By $HOSTNAME;
-    root   /etc/nginx/html;
+    root   /var/www/html;
     index  index.html index.htm;
-
     location /hbnb_static {
-     alias /data/web_static/current;
-     index index.html index.htm;
+        alias /data/web_static/current;
+        index index.html index.htm;
     }
-
     location /redirect_me {
-        return 301 https://github.com/saidskander/;
+        return 301 http://cuberule.com/;
     }
     error_page 404 /404.html;
     location /404 {
@@ -38,4 +31,5 @@ printf %s "server {
       internal;
     }
 }" > /etc/nginx/sites-available/default
+
 service nginx restart
