1d6b8c6b57eb2ab42a8b3d0666a013cf9f316ae1
saidskander <askander199h@outlook.com>
2021-01-23 16:52:10 +0200

    Done

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -10,5 +10,10 @@ def hello_hbnb():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """ello HBNB!"""
+    return "Hello HBNB!"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
